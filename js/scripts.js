5e27ba199328ecb886412df232d4fbb37b87b0fe
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-01-26 09:52:43 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,35 @@ function calculate(order) {
         result = add(firstInput, secondInput);
     } else if (order === 'subtract') {
         result = subtract(firstInput, secondInput);
+    } else if (order === 'multiply') {
+        result = multiply(firstInput, secondInput);
+    } else if (order === 'divide') {
+        result = divide(firstInput, secondInput);
     }
+    
 
     document.getElementById('final-result').innerText = result;
 }
 
-function add(a, b){
+function add(a, b) {
     return a + b;
 }
 
 function subtract(a, b) {
-
+    return a-b;
 }
 
 function multiply(a, b) {
+    return a * b;
 
 }
 
 function divide(a, b) {
+    return a/b;
 
+}
+
+function pivalue() {
+    var pivalue = 3.14;
+    console.log(pivalue);
 }
