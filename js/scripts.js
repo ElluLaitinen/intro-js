0e2991789bbfd17190fde33dd7f6483b6c00f558
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-02-02 18:12:15 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,15 @@
 
   // Set event listeners
   function setEventListeners() {
-    next = $("slider_button_next")[0];
-    prev = $("slider_button_prev")[0];
+   var next = $(".slider__button--next")[0];
+   var prev = $(".slider__button--prev")[0];
     /*var next = document.getElementsByClassName("slider__button--next")[0],
       prev = document.getElementsByClassName("slider__button--prev")[0];*/
-
-    $(next).on("click", getNext);
-    $(prev).on("click", getPrev);
+console.log(next);
+console.log(totalItems);
+console.log(items);
+    $(next).on("click", getNext());
+    $(prev).on("click", getPrev());
 
     /*next.addEventListener("click", getNext);
     prev.addEventListener("click", getPrev);*/
@@ -20,7 +22,7 @@
 
   function getNext() {
     items[slide].classList.remove("active");
-
+console.log("next" + slide);
     if (slide === totalItems - 1) {
       slide = 0;
     } else {
